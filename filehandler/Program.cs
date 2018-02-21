4f06dc91899bdde7f0f1bca42f0e390cc9711fc3
Euan Garden <euan.garden@gmail.com>
2018-02-20 22:17:30 -0800

    New project to debug .psc files which seem to be new normal

---
+++ filehandler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FileHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipPath = @"C:\dev\data\IDPAScoring\TestPSC.psc";
+            string targetFileName = "match_def.json";
+
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName == targetFileName)
+                    {
+                        using (var zipStream = entry.Open()){
+                            StreamReader reader = new StreamReader( zipStream );
+                            string data = reader.ReadToEnd();
+                            JObject shooterData = JObject.Parse(data);
+                            Console.WriteLine(shooterData);
+                            Console.ReadLine();
+                        }
+                    }
+                }
+            } 
+        }
+    }
+}
